0fa4014acc7a6bbbd014fa28a37cdcf76132c325
ABC <abc@gmail.com>
2025-07-22 10:42:54 +0000

    c project

---
+++ big2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+biggest2() {
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    if (a > b) {
+        printf("The biggest number is: %d\n", a);
+    } else {
+        printf("The biggest number is: %d\n", b);
+    }
+
+   //return 0;
+}
